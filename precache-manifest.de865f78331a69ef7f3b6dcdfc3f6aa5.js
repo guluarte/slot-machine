1ae037772a2676241324528608b7bb3b61335f1a
Rodolfo Guluarte Hale <guluarte@gmail.com>
2018-12-11 16:01:18 -0700

    Updates

---
+++ precache-manifest.de865f78331a69ef7f3b6dcdfc3f6aa5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2613183a7e2eff632da5c897117ceab2",
+    "url": "/slot-machine/static/media/slot-bg.2613183a.png"
+  },
+  {
+    "revision": "0fcee238929d264012971cae5c41f52b",
+    "url": "/slot-machine/static/media/reel.0fcee238.png"
+  },
+  {
+    "revision": "f02b815ccd64309c2c72a254e8c0c617",
+    "url": "/slot-machine/static/media/bg.f02b815c.jpg"
+  },
+  {
+    "revision": "565a57790e190fec06a5793e260d809f",
+    "url": "/slot-machine/static/media/Cherry.565a5779.png"
+  },
+  {
+    "revision": "2d931611f4844b33da7e",
+    "url": "/slot-machine/static/js/runtime~main.2d931611.js"
+  },
+  {
+    "revision": "154f3da22570cdf86ad2",
+    "url": "/slot-machine/static/js/main.154f3da2.chunk.js"
+  },
+  {
+    "revision": "14c3edf8440cd8e4d2a6",
+    "url": "/slot-machine/static/js/1.14c3edf8.chunk.js"
+  },
+  {
+    "revision": "154f3da22570cdf86ad2",
+    "url": "/slot-machine/static/css/main.6505542e.chunk.css"
+  },
+  {
+    "revision": "af4b4ee91b04735296f0911077bf3356",
+    "url": "/slot-machine/index.html"
+  }
+];
